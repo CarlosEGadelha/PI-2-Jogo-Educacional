70e2090be02e3d5f60002a8083f99c4e10c26af8
Username <andrenakayama350@gmail.com>
2020-10-12 18:24:15 -0300

    Botões do menu chamando as suas respectivas funções - Função para Tutorial e para Desenvolvedores criado

--- Jogo/Jogo.c
+++ Jogo/Jogo.c
@@ -5,6 +5,7 @@
 #include <allegro5/allegro_native_dialog.h>
 #include <allegro5/allegro_audio.h>
 #include <allegro5/allegro_acodec.h>
+#include <stdio.h>
 
 //fps = frames per second = atualizacoes de tela por segundo
 #define FPS 60.0
@@ -27,7 +28,9 @@ ALLEGRO_BITMAP* imagem = NULL;
 ALLEGRO_BITMAP* botao_sair = NULL;
 ALLEGRO_BITMAP* jogar = 0;
 ALLEGRO_BITMAP* instrucoes = 0;
-ALLEGRO_BITMAP* desenvolvedores = 0;
+ALLEGRO_BITMAP* menuDesenvolvedores = 0;
+ALLEGRO_BITMAP* voltarMenu = 0;
+ALLEGRO_BITMAP* fonteTitulo = 0;
 
 void error_msg(char* text) {
     al_show_native_message_box(janela, "ERRO",
@@ -397,7 +400,236 @@ int jogo(void) {
     return 0;
 }
 
+
+//Função Tutorial
+int tutorial() {
+    al_destroy_display(janela);
+    ALLEGRO_DISPLAY* janela = NULL;
+    ALLEGRO_FONT* fonte = NULL;
+
+    if (!al_init()) {
+        error_msg("Falha ao inicializar a Allegro");
+        return -1;
+    }
+
+    al_init_font_addon();
+
+    if (!al_init_image_addon()) {
+        error_msg("Falha ao inicializar add-on allegro_image");
+        return -1;
+    }
+
+    if (!al_init_ttf_addon()) {
+        error_msg("Falha ao inicializar add-on allegro_ttf");
+        return -1;
+    }
+
+    fonte = al_load_font("arial.ttf", 32, 0);
+    if (!fonte) {
+        al_destroy_display(janela);
+        error_msg("Falha ao carregar fonte");
+        return -1;
+    }
+
+    fonteTitulo = al_load_font("arial.ttf", 42, 0);
+    if (!fonte) {
+        al_destroy_display(janela);
+        error_msg("Falha ao carregar fonte");
+        return -1;
+    }
+
+    janela = al_create_display(LARGURA_TELA, ALTURA_TELA);
+    if (!janela) {
+        error_msg("Falha ao criar janela");
+        return -1;
+    }
+    al_set_window_title(janela, "TUTORIAL");
+
+    if (!al_install_mouse()) {
+        error_msg("Falha ao inicializar o mouse");
+        al_destroy_display(janela);
+        return -1;
+    }
+
+    if (!al_set_system_mouse_cursor(janela, ALLEGRO_SYSTEM_MOUSE_CURSOR_DEFAULT)) {
+        error_msg("Falha ao atribuir ponteiro do mouse");
+        al_destroy_display(janela);
+        return -1;
+    }
+
+    voltarMenu = al_load_bitmap("sprites/botao_quit.bmp");
+    if (!voltarMenu) {
+        error_msg("Falha ao criar botão de saída");
+        al_destroy_display(janela);
+        return 0;
+    }
+
+    fila_eventos = al_create_event_queue();
+    if (!fila_eventos) {
+        error_msg("Falha ao criar fila de eventos");
+        al_destroy_display(janela);
+        return 0;
+    }
+
+    al_register_event_source(fila_eventos, al_get_mouse_event_source());
+    al_register_event_source(fila_eventos, al_get_display_event_source(janela));
+
+    int sair = 0;
+
+    while (!sair) {
+        al_draw_bitmap(imagem, 0, 0, 0);
+        while (!al_is_event_queue_empty(fila_eventos)) {
+            ALLEGRO_EVENT evento;
+            al_wait_for_event(fila_eventos, &evento);
+            if (evento.type == ALLEGRO_EVENT_MOUSE_BUTTON_UP) {
+                if (evento.mouse.x >= 10 &&
+                    evento.mouse.x <= 210 &&
+                    evento.mouse.y >= 10 &&
+                    evento.mouse.y <= 60) {
+                    sair = 1;
+                    menu();
+                }
+            }
+        }
+
+        // Preenchemos a tela com a cor ciza
+        al_clear_to_color(al_map_rgb(80, 80, 80));
+
+        // Texto tutorial
+        al_draw_text(fonte, al_map_rgb(0, 0, 0), LARGURA_TELA / 2, 100, ALLEGRO_ALIGN_CENTRE, "TUTORIAL");
+        al_draw_textf(fonte, al_map_rgb(0, 0, 0), LARGURA_TELA / 2, 200, ALLEGRO_ALIGN_CENTRE, "MOVIMENTACAO: W.A.S.D");
+        al_draw_textf(fonte, al_map_rgb(0, 0, 0), LARGURA_TELA / 2, 250,
+            ALLEGRO_ALIGN_CENTRE, "INTERACAO NA TELA DE COMBATE: A (Direita)");
+        al_draw_textf(fonte, al_map_rgb(0, 0, 0), LARGURA_TELA / 2, 300, ALLEGRO_ALIGN_CENTRE, "B(Esquerda) - ENTER(Selecionar)");
+
+        al_set_target_bitmap(voltarMenu);
+
+        al_set_target_bitmap(al_get_backbuffer(janela));
+        al_draw_bitmap(voltarMenu, 10, 10, 0);
+
+        al_flip_display();
+    }
+
+    return 0;
+}
+
+
+//Função Desenvolvedores
+int desenvolvedores() {
+    al_destroy_display(janela);
+    ALLEGRO_DISPLAY* janela = NULL;
+    ALLEGRO_FONT* fonte = NULL;
+
+    if (!al_init()) {
+        error_msg("Falha ao inicializar a Allegro");
+        return -1;
+    }
+
+    al_init_font_addon();
+
+    if (!al_init_image_addon()) {
+        error_msg("Falha ao inicializar add-on allegro_image");
+        return -1;
+    }
+
+    if (!al_init_ttf_addon()) {
+        error_msg("Falha ao inicializar add-on allegro_ttf");
+        return -1;
+    }
+
+    fonte = al_load_font("arial.ttf", 32, 0);
+    if (!fonte) {
+        al_destroy_display(janela);
+        error_msg("Falha ao carregar fonte");
+        return -1;
+    }
+
+    fonteTitulo = al_load_font("arial.ttf", 42, 0);
+    if (!fonte) {
+        al_destroy_display(janela);
+        error_msg("Falha ao carregar fonte");
+        return -1;
+    }
+
+    janela = al_create_display(LARGURA_TELA, ALTURA_TELA);
+    if (!janela) {
+        error_msg("Falha ao criar janela");
+        return -1;
+    }
+    al_set_window_title(janela, "DESENVOLVEDORES");
+
+    if (!al_install_mouse()) {
+        error_msg("Falha ao inicializar o mouse");
+        al_destroy_display(janela);
+        return -1;
+    }
+
+    if (!al_set_system_mouse_cursor(janela, ALLEGRO_SYSTEM_MOUSE_CURSOR_DEFAULT)) {
+        error_msg("Falha ao atribuir ponteiro do mouse");
+        al_destroy_display(janela);
+        return -1;
+    }
+
+    voltarMenu = al_load_bitmap("sprites/botao_quit.bmp");
+    if (!voltarMenu) {
+        error_msg("Falha ao criar botão de saída");
+        al_destroy_display(janela);
+        return 0;
+    }
+
+    fila_eventos = al_create_event_queue();
+    if (!fila_eventos) {
+        error_msg("Falha ao criar fila de eventos");
+        al_destroy_display(janela);
+        return 0;
+    }
+
+    al_register_event_source(fila_eventos, al_get_mouse_event_source());
+    al_register_event_source(fila_eventos, al_get_display_event_source(janela));
+
+    int sair = 0;
+
+    while (!sair) {
+        al_draw_bitmap(imagem, 0, 0, 0);
+        while (!al_is_event_queue_empty(fila_eventos)) {
+            ALLEGRO_EVENT evento;
+            al_wait_for_event(fila_eventos, &evento);
+            if (evento.type == ALLEGRO_EVENT_MOUSE_BUTTON_UP) {
+                if (evento.mouse.x >= 10 &&
+                    evento.mouse.x <= 210 &&
+                    evento.mouse.y >= 10 &&
+                    evento.mouse.y <= 60) {
+                    sair = 1;
+                    menu();
+                }
+            }
+        }
+
+        // Preenchemos a tela com a cor ciza
+        al_clear_to_color(al_map_rgb(80, 80, 80));
+
+        // Texto tutorial
+        al_draw_text(fonte, al_map_rgb(0, 0, 0), LARGURA_TELA / 2, 100, ALLEGRO_ALIGN_CENTRE, "DESENVOLVEDORES");
+        al_draw_textf(fonte, al_map_rgb(0, 0, 0), LARGURA_TELA / 2, 200, ALLEGRO_ALIGN_CENTRE, "Andre Tomio Nakayama");
+        al_draw_textf(fonte, al_map_rgb(0, 0, 0), LARGURA_TELA / 2, 250, ALLEGRO_ALIGN_CENTRE, "Carlos Eduardo Gadelha Silva");
+        al_draw_textf(fonte, al_map_rgb(0, 0, 0), LARGURA_TELA / 2, 300, ALLEGRO_ALIGN_CENTRE, "Rafael Ribas Albuquerque");
+
+        al_set_target_bitmap(voltarMenu);
+
+        al_set_target_bitmap(al_get_backbuffer(janela));
+        al_draw_bitmap(voltarMenu, 10, 10, 0);
+
+        al_flip_display();
+    }
+
+    return 0;
+}
+
+
+//Função Menu
 int menu() {
+    al_destroy_display(janela);
+
     if (!al_init()) {
         error_msg("Falha ao inicializar a Allegro");
         return -1;
@@ -462,8 +694,8 @@ int menu() {
         return 0;
     }
 
-    desenvolvedores = al_load_bitmap("sprites/botao_credito.bmp");
-    if (!desenvolvedores) {
+    menuDesenvolvedores = al_load_bitmap("sprites/botao_credito.bmp");
+    if (!menuDesenvolvedores) {
         error_msg("Falha ao carregar o arquivo de imagem");
         al_destroy_display(janela);
         return 0;
@@ -474,7 +706,7 @@ int menu() {
         error_msg("Falha ao criar botão de saída");
         al_destroy_bitmap(jogar);
         al_destroy_bitmap(instrucoes);
-        al_destroy_bitmap(desenvolvedores);
+        al_destroy_bitmap(menuDesenvolvedores);
         al_destroy_display(janela);
         return 0;
     }
@@ -490,9 +722,6 @@ int menu() {
     al_register_event_source(fila_eventos, al_get_display_event_source(janela));
 
     int sair = 0;
-    int menuJogar = 0;
-    int menuInstrucoes = 0;
-    int menuDesenvolvedores = 0;
 
     while (!sair) {
         al_draw_bitmap(imagem, 0, 0, 0);
@@ -504,32 +733,24 @@ int menu() {
                     evento.mouse.x <= 500 &&
                     evento.mouse.y >= 150 &&
                     evento.mouse.y <= 200) {
-                    menuJogar = 1;
                     sair = 1;
                     jogo();
-                }
-                else {
-                    menuJogar = 0;
                 }
 
                 if (evento.mouse.x >= 300 &&
                     evento.mouse.x <= 500 &&
                     evento.mouse.y >= 230 &&
                     evento.mouse.y <= 280) {
-                    menuInstrucoes = 1;
-                }
-                else {
-                    menuInstrucoes = 0;
+                    sair = 1;
+                    tutorial();
                 }
 
                 if (evento.mouse.x >= 300 &&
                     evento.mouse.x <= 500 &&
                     evento.mouse.y >= 310 &&
                     evento.mouse.y <= 360) {
-                    menuDesenvolvedores = 1;
-                }
-                else {
-                    menuDesenvolvedores = 0;
+                    sair = 1;
+                    desenvolvedores();
                 }
 
                 if (evento.mouse.x >= 300 &&
@@ -546,7 +767,7 @@ int menu() {
         al_set_target_bitmap(al_get_backbuffer(janela));
         al_draw_bitmap(jogar, 300, 150, 0);
         al_draw_bitmap(instrucoes, 300, 230, 0);
-        al_draw_bitmap(desenvolvedores, 300, 310, 0);
+        al_draw_bitmap(menuDesenvolvedores, 300, 310, 0);
         al_draw_bitmap(botao_sair, 300, 390, 0);
 
         al_flip_display();
@@ -555,7 +776,7 @@ int menu() {
     al_destroy_bitmap(botao_sair);
     al_destroy_bitmap(jogar);
     al_destroy_bitmap(instrucoes);
-    al_destroy_bitmap(desenvolvedores);
+    al_destroy_bitmap(menuDesenvolvedores);
     al_destroy_display(janela);
     al_destroy_event_queue(fila_eventos);
 
